b7d2e8dfad5c59f298e5aaf83e3f8c7ca00103d9
Antonio Bulgheroni <antonio.bulgheroni@gmail.com>
2022-07-02 11:12:28 +0200

    Renamed the readme file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'Readme.md')).read()
+README = open(os.path.join(here, 'README.md')).read()
 
 setup(name='elog',
       version='1.3.11',
