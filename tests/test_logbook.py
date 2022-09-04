65cb40501f1ffe1304d4e4c0fc1fd61debc9c0a1
Antonio Bulgheroni <antonio.bulgheroni@gmail.com>
2022-09-04 17:02:30 +0200

    Add a test case to demonstrate the new capability.

--- tests/test_logbook.py
+++ tests/test_logbook.py
@@ -4,40 +4,25 @@ import elog
 
 
 
-
 class TestClass(unittest.TestCase):
 
     # TODO add test for delete
     # TODO add description how to run the test docker container for testing
 
     elog_hostname = 'https://elog.psi.ch/elogs/Linux+Demo/'
-
+    message = 'This message text is new'
 
     def test_get_message_ids(self):
         logbook = elog.open(self.elog_hostname)
         message_ids = logbook.get_message_ids()
-        print(len(message_ids))
-        print(message_ids)
-
+        
     def test_get_last_message_id(self):
 
         logbook = elog.open(self.elog_hostname)
-        msg_id = logbook.post('This is message text is new', attributes={'Author':'AB', 'Type':'Routine'})
+        msg_id = logbook.post(self.message, attributes={'Author':'AB', 'Type':'Routine'})
         message_id = logbook.get_last_message_id()
-
-        print(msg_id)
-        print(message_id)
         self.assertEqual(msg_id, message_id, "Created message does not show up as last edited message")
         
-
-    def test_read(self):
-
-        logbook = elog.open(self.elog_hostname)
-        message, attributes, attachments = logbook.read(logbook.get_last_message_id())
-        print(message)
-        self.assertEqual(message, 'This is message text is new', "Unable to retrieve message")
-
-
     def test_edit(self):
         logbook = elog.open(self.elog_hostname)
         logbook.post('hehehehehe', msg_id=logbook.get_last_message_id(), attributes={"Subject": 'py_elog test [mod]'})
@@ -45,17 +30,36 @@ class TestClass(unittest.TestCase):
     def test_search(self):
         logbook = elog.open(self.elog_hostname)
         ids = logbook.search("message")
-        print(ids)
         
     def test_search_empty(self):
         logbook = elog.open(self.elog_hostname)
         ids = logbook.search("")
-        print(ids)
         
     def test_search_dict(self):
         logbook = elog.open(self.elog_hostname)
         ids = logbook.search({"Category": "Hardware"})
-        print(ids)
+        
+    def test_post_special_characters(self):
+        logbook = elog.open(self.elog_hostname)
+        attributes = { 'Author' : 'Me', 'Type' : 'Other', 'Category' : 'General', 
+                      'Subject' : 'This is a test of UTF-8 characters like èéöä'}
+        message = 'Just to be clear this is a general test using UTF-8 characters like èéöä.'  
+        msg_id = logbook.post(message,  reply=False, attributes=attributes,encoding='HTML')
+        read_msg, read_attr, read_att = logbook.read(msg_id)
+        
+        mess_ok = message == read_msg
 
+        attr_ok = True
+        for key in attributes:
+            if attributes[key] == read_attr[key]:
+                attr_ok = attr_ok and True
+            else:
+                attr_ok = attr_ok and False
+
+        whole_test = attr_ok and mess_ok
+        
+        self.assertTrue(whole_test)
+        
 if __name__ == '__main__':
     unittest.main()
+    
