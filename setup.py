800620a2343c0200c3a38f8a8e3e0c47579cb6a6
ebner <simon.ebner@psi.ch>
2022-11-08 09:21:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, "README.md")).read()
 
-setup(name='py_elog',
-      version='1.3.14',
-      description="Python library to access Elog.",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      author='Paul Scherrer Institute (PSI)',
-      url="https://github.com/paulscherrerinstitute/py_elog",
-      keywords='elog, electronic, logbook',
-      packages=['elog'],
-      install_requires=[
-          'requests', 'passlib', 'lxml'
-      ],
-      )
+setup(
+    name="py_elog",
+    version="1.3.15",
+    description="Python library to access Elog.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Paul Scherrer Institute (PSI)",
+    url="https://github.com/paulscherrerinstitute/py_elog",
+    keywords="elog, electronic, logbook",
+    packages=["elog"],
+    install_requires=["requests", "passlib", "lxml"],
+)
